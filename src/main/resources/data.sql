028e01d4d318f5e74626ba2eb4b4be7e2b1a5702
windyedwin <kimhyungu@nate.com>
2024-05-16 06:12:16 +0900

    1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,7 +7,7 @@ insert into user_account (user_id, user_password, nickname, email, memo, created
 (( '{noop}asdf1234', 'Uno2', 'uno2@mail.com', 'I am Uno2.', now(), ( now(), 'uno2')
 ;
 */
--- 123 게시글
+-- 122 게시글
 insert into article (title, content, hashtag, created_by, modified_by, created_at, modified_at) values
 ( 'Quisque ut erat.', 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
 
